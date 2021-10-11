aafc33b41f7309e7712dab0193f0ffa0ccdfa4ad
Salena Echols <salucky9@gmail.com>
2021-10-11 19:54:00 -0400

    save for night

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// debugger;
+
+var el = document.getElementById('cards');
+
+
+function callTheLoop() {
+    console.log('call the loop');
+
+    runTheLoop();
+}
+
+function runTheLoop() {
+    for ( var i = 0; i < 10; i++) {
+        console.log(i);
+    }
+}
+
+callTheLoop();
