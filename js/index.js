69bdd48b8be7b6d68fda95add71af8ed97e54f2e
Salena Echols <salucky9@gmail.com>
2021-10-12 20:40:38 -0400

    wk5 d2

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
-// debugger;
-
-var el = document.getElementById('cards');
 
 
-function callTheLoop() {
-    console.log('call the loop');
 
-    runTheLoop();
-}
 
-function runTheLoop() {
-    for ( var i = 0; i < 10; i++) {
-        console.log(i);
-    }
-}
+// // debugger;
 
-callTheLoop();
+// var el = document.getElementById('cards');
+
+
+// function callTheLoop() {
+//     console.log('call the loop');
+
+//     runTheLoop();
+// }
+
+// function runTheLoop() {
+//     for ( var i = 0; i < 10; i++) {
+//         console.log(i);
+//     }
+// }
+
+// callTheLoop();
